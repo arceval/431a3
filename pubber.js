320f0edfe3f5fe4a59b4f25636dcbc5018a96d0a
Rohm Laxton <rohmkl@gmail.com>
2019-02-19 03:04:49 +0000

    Added comments

--- pubber.js
+++ pubber.js
@@ -1,11 +1,13 @@
 var zmq = require('zeromq');
 var sock = zmq.socket('pub');
 
+// Binds the publisher to port 3000
 sock.bindSync('tcp://*:3000');
 console.log('Publisher bound on port 3000');
 
+// Sends the timestamp out to subscribers ever half-second
 setInterval (function() {
-    console.log('Sending time to subscribe port');
     var timestamp = new Date();
     sock.send(['time', timestamp.getTime()]);
+    console.log('timestamp sent to subscribers', timestamp);
 }, 500);
